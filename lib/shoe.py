7e6d4552503fd9b4a17b5c8d0af48adf8b4da07e
joewaweruwanjema <wawerujoe@gmail.com>
2025-08-10 17:57:34 +0300

    add

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if isinstance(new_size, int):
+            self._size = new_size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+
+stan_smith = Shoe("Adidas", 9)
